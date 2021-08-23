a997057379b0a385cee31ae50458dcb9c0664b94
Grig Alex <alexengrigdev@gmail.com>
2021-08-24 00:23:58 +0300

    Update tests

--- src/test/java/dev/alexengrig/util/ConditionalTest.java
+++ src/test/java/dev/alexengrig/util/ConditionalTest.java
@@ -11,6 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,9 +22,13 @@ class ConditionalTest {
         void should_check_success_case();
 
         default void should_check_failure_case() {
+            throw new UnsupportedOperationException();
         }
     }
 
+    /**
+     * @see Conditional#empty()
+     */
     @Nested
     class EmptyMethodTest implements MethodTest {
 
@@ -34,6 +39,9 @@ class ConditionalTest {
         }
     }
 
+    /**
+     * @see Conditional#of(java.lang.Object)
+     */
     @Nested
     class OfMethodTest implements MethodTest {
 
@@ -45,6 +53,9 @@ class ConditionalTest {
         }
     }
 
+    /**
+     * @see Conditional#ofOptional(java.util.Optional)
+     */
     @Nested
     class OfOptionalMethodTest implements MethodTest {
 
@@ -64,6 +75,22 @@ class ConditionalTest {
         }
     }
 
+    /**
+     * @see Conditional#hasNo()
+     */
+    @Nested
+    class HasNoMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+
+        }
+    }
+
+    /**
+     * @see Conditional#has()
+     */
     @Nested
     class HasMethodTest implements MethodTest {
 
@@ -75,16 +102,9 @@ class ConditionalTest {
         }
     }
 
-    @Nested
-    class HasNoMethodTest implements MethodTest {
-
-        @Test
-        @Override
-        public void should_check_success_case() {
-
-        }
-    }
-
+    /**
+     * @see Conditional#ifHas(java.util.function.Predicate, java.util.function.Consumer)
+     */
     @Nested
     class IfHasMethodTest implements MethodTest {
 
@@ -105,6 +125,9 @@ class ConditionalTest {
         }
     }
 
+    /**
+     * @see Conditional#ifHasOrElse(java.util.function.Predicate, java.util.function.Consumer, java.lang.Runnable)
+     */
     @Nested
     class IfHasOrElseMethodTest implements MethodTest {
 
@@ -125,68 +148,281 @@ class ConditionalTest {
             assertThrows(NullPointerException.class, () ->
                     Conditional.empty().ifHasOrElse(Objects::isNull, null, null));
             assertThrows(NullPointerException.class, () ->
-                    Conditional.empty().ifHasOrElse(Objects::isNull, r -> {}, null));
+                    Conditional.empty().ifHasOrElse(Objects::isNull, r -> {
+                    }, null));
         }
     }
-//TODO add other tests
+
+    /**
+     * @see Conditional#filter(java.util.function.Predicate)
+     */
+    @Nested
+    class FilterMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertTrue(Conditional.of("string").filter(string -> string.startsWith("s")).has());
+            assertTrue(Conditional.of("string").filter(string -> string.startsWith("g")).hasNo());
+            assertTrue(Conditional.empty().filter(s -> fail()).hasNo());
+        }
+
+        @Test
+        @Override
+        public void should_check_failure_case() {
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.empty().filter(null));
+        }
+    }
+
+    /**
+     * @see Conditional#evaluate(java.util.function.Predicate)
+     */
+    @Nested
+    class EvaluateMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertTrue(Conditional.of("string").evaluate(Conditional::hasNo).hasNo());
+            assertTrue(Conditional.of("string").evaluate(Conditional::has).has());
+            assertTrue(Conditional.empty().evaluate(c -> fail()).hasNo());
+        }
+
+        @Test
+        @Override
+        public void should_check_failure_case() {
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.of("string").evaluate(null));
+        }
+    }
+
+    /**
+     * @see Conditional#map(java.util.function.Function)
+     */
+    @Nested
+    class MapMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertTrue(Conditional.of("string").map(String::length).test(l -> l == 6));
+            assertTrue(Conditional.empty().map(o -> fail()).hasNo());
+        }
+
+        @Test
+        @Override
+        public void should_check_failure_case() {
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.empty().map(null));
+        }
+    }
+
+    /**
+     * @see Conditional#flatMap(java.util.function.Function)
+     */
+    @Nested
+    class FlatMapMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertTrue(Conditional.of("string").flatMap(s -> Conditional.of(s.length())).test(l -> l == 6));
+            assertTrue(Conditional.empty().flatMap(o -> fail()).hasNo());
+        }
+
+        @Test
+        @Override
+        public void should_check_failure_case() {
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.empty().flatMap(null));
+        }
+    }
+
+    /**
+     * @see Conditional#flatMapOptional(java.util.function.Function)
+     */
+    @Nested
+    class FlatMapOptionalMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertTrue(Conditional.of("string").flatMapOptional(s -> Optional.of(s.length())).test(l -> l == 6));
+            assertTrue(Conditional.empty().flatMapOptional(o -> fail()).hasNo());
+        }
+
+        @Test
+        @Override
+        public void should_check_failure_case() {
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.empty().flatMapOptional(null));
+        }
+    }
+
+    /**
+     * @see Conditional#or(java.util.function.Supplier)
+     */
+    @Nested
+    class OrMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertTrue(Conditional.of("string1").or(() -> Conditional.of("string2")).test("string1"::equals));
+            assertTrue(Conditional.<String>empty().or(() -> Conditional.of("string")).has());
+        }
+
+        @Test
+        @Override
+        public void should_check_failure_case() {
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.empty().or(null));
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.empty().or(() -> null));
+        }
+    }
+
+    /**
+     * @see Conditional#optional()
+     */
+    @Nested
+    class OptionalMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            Optional<String> optional = Conditional.of("string").optional();
+            assertTrue(optional.isPresent());
+            assertEquals("string", optional.get());
+
+            optional = Conditional.<String>empty().optional();
+            assertFalse(optional.isPresent());
+        }
+    }
+
+    /**
+     * @see Conditional#stream()
+     */
+    @Nested
+    class StreamMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            Stream<String> stream = Conditional.of("string").stream();
+            Optional<String> optional = stream.findFirst();
+            assertTrue(optional.isPresent());
+            assertEquals("string", optional.get());
+
+            stream = Conditional.<String>empty().stream();
+            optional = stream.findFirst();
+            assertFalse(optional.isPresent());
+        }
+    }
+
+    /**
+     * @see Conditional#test(java.util.function.Predicate)
+     */
+    @Nested
+    class TestMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertTrue(Conditional.of("string").test(s -> !s.isEmpty()));
+            assertFalse(Conditional.empty().test(s -> fail()));
+        }
+
+        @Test
+        @Override
+        public void should_check_failure_case() {
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.empty().test(null));
+        }
+    }
+
+    /**
+     * @see Conditional#orElse(java.util.function.Predicate, boolean)
+     */
     @Nested
     class OrElseMethodTest implements MethodTest {
 
+        @Test
         @Override
         public void should_check_success_case() {
             assertFalse(Conditional.of("string").orElse(String::isEmpty, true));
             assertTrue(Conditional.<String>empty().orElse(String::isEmpty, true));
         }
 
+        @Test
         @Override
         public void should_check_failure_case() {
-            assertThrows(NullPointerException.class, () -> Conditional.empty().orElse(null, true));
+            assertThrows(NullPointerException.class, () ->
+                    Conditional.empty().orElse(null, true));
         }
     }
 
+    /**
+     * @see Conditional#orElseTrue(java.util.function.Predicate)
+     */
     @Nested
     class OrElseTrueMethodTest implements MethodTest {
 
+        @Test
         @Override
         public void should_check_success_case() {
             assertFalse(Conditional.of("string").orElseTrue(String::isEmpty));
             assertTrue(Conditional.<String>empty().orElseTrue(String::isEmpty));
         }
 
+        @Test
         @Override
         public void should_check_failure_case() {
             assertThrows(NullPointerException.class, () -> Conditional.empty().orElseTrue(null));
         }
     }
 
+    /**
+     * @see Conditional#orElseFalse(java.util.function.Predicate)
+     */
     @Nested
     class OrElseFalseMethodTest implements MethodTest {
 
+        @Test
         @Override
         public void should_check_success_case() {
             assertFalse(Conditional.of("string").orElseFalse(String::isEmpty));
             assertFalse(Conditional.<String>empty().orElseFalse(String::isEmpty));
         }
 
+        @Test
         @Override
         public void should_check_failure_case() {
             assertThrows(NullPointerException.class, () -> Conditional.empty().orElseFalse(null));
         }
     }
 
+    /**
+     * @see Conditional#orElseGet(java.util.function.Predicate, java.util.function.BooleanSupplier)
+     * @see Conditional#orElseGet(java.util.function.Predicate, java.util.function.Supplier)
+     */
     @Nested
     class OrElseGetMethodTest implements MethodTest {
 
+        @Test
         @Override
         public void should_check_success_case() {
             assertFalse(Conditional.of("string").orElseGet(String::isEmpty, () -> true));
             assertTrue(Conditional.<String>empty().orElseGet(String::isEmpty, () -> true));
 
             BooleanSupplier trueSupplier = this::trueSupplier;
+            assertTrue(Conditional.of("").orElseGet(String::isEmpty, trueSupplier));
             assertTrue(Conditional.<String>empty().orElseGet(String::isEmpty, trueSupplier));
             assertTrue(Conditional.<String>empty().orElseGet(String::isEmpty, this::trueSupplier));
 
             Supplier<Boolean> boxedTrueSupplier = this::boxedTrueSupplier;
+            assertTrue(Conditional.of("").orElseGet(String::isEmpty, boxedTrueSupplier));
             assertTrue(Conditional.<String>empty().orElseGet(String::isEmpty, boxedTrueSupplier));
             assertTrue(Conditional.<String>empty().orElseGet(String::isEmpty, this::boxedTrueSupplier));
         }
@@ -195,10 +431,12 @@ class ConditionalTest {
             return true;
         }
 
+        @Test
         private Boolean boxedTrueSupplier() {
             return true;
         }
 
+        @Test
         @Override
         public void should_check_failure_case() {
             assertThrows(NullPointerException.class, () ->
@@ -212,16 +450,25 @@ class ConditionalTest {
         }
     }
 
+    /**
+     * @see Conditional#orElseThrow(java.util.function.Predicate)
+     * @see Conditional#orElseThrow(java.util.function.Predicate, java.util.function.Supplier)
+     */
     @Nested
     class OrElseThrowMethodTest implements MethodTest {
 
+        @Test
         @Override
         public void should_check_success_case() {
             assertFalse(Conditional.of("string").orElseThrow(String::isEmpty));
+            assertFalse(Conditional.of("string").orElseThrow(String::isEmpty, AssertionError::new));
             assertThrows(NoSuchElementException.class, () ->
                     Conditional.<String>empty().orElseThrow(String::isEmpty));
+            assertThrows(AssertionError.class, () ->
+                    Conditional.<String>empty().orElseThrow(String::isEmpty, AssertionError::new));
         }
 
+        @Test
         @Override
         public void should_check_failure_case() {
             assertThrows(NullPointerException.class, () ->
@@ -229,16 +476,47 @@ class ConditionalTest {
         }
     }
 
-    static class User {
+    /**
+     * @see Conditional#equals(java.lang.Object)
+     */
+    @Nested
+    class EqualsMethodTest implements MethodTest {
 
-        private final String name;
-
-        public User(String name) {
-            this.name = name;
+        @Test
+        @Override
+        public void should_check_success_case() {
+            Conditional<String> conditional = Conditional.of("string");
+            assertEquals(conditional, conditional);
+            assertNotEquals(conditional, new Object());
+            assertEquals(conditional, Conditional.of("string"));
         }
+    }
 
-        public String getName() {
-            return name;
+    /**
+     * @see Conditional#hashCode()
+     */
+    @Nested
+    class HashCodeMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertEquals("string".hashCode(), Conditional.of("string").hashCode());
+            assertEquals(0, Conditional.empty().hashCode());
+        }
+    }
+
+    /**
+     * @see Conditional#toString()
+     */
+    @Nested
+    class ToStringMethodTest implements MethodTest {
+
+        @Test
+        @Override
+        public void should_check_success_case() {
+            assertEquals("Conditional[string]", Conditional.of("string").toString());
+            assertEquals("Conditional.empty", Conditional.empty().toString());
         }
     }
 }
