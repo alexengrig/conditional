a92737e461ac02075e645cabe975a00e209955a1
Grig Alex <alexengrigdev@gmail.com>
2021-08-24 23:14:27 +0300

    Create package-info.java

---
+++ src/main/java/dev/alexengrig/util/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This library has the utility class - {@code Conditional},
+ * it is like {@code Optional<Boolean>}.
+ *
+ * @version 0.1.0
+ * @author Grig Alex
+ * @see dev.alexengrig.util.Conditional
+ * @see java.util.Optional
+ * @since 0.1.0
+ */
+package dev.alexengrig.util;
